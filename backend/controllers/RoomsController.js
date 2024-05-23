d0f92cb85be330f646164061dcdfa07a4e4e5b4c
niialabi <taufiq.alabi@gmail.com>
2024-05-23 02:29:42 +0000

    Implement user duplication check in rooms with inline comments for routes

--- backend/controllers/RoomsController.js
+++ backend/controllers/RoomsController.js
@@ -3,9 +3,11 @@ import * as userUtils from "../utils/users.js";
 
 export async function getRooms(req, res) {
   try {
+    // check's the X-Token in the request and get the user info
     const user = await userUtils.validateUser(req);
     if (!user) return res.status(401).send({ error: "Unauthorized" });
     if (!user.rooms) return res.status(200).send({ rooms: [] });
+    // get an array of room models that the user has joined or created.
     const rooms = await dbClient.getUserRooms(user.rooms);
     return res.status(200).send({ rooms });
   } catch (error) {
@@ -20,8 +22,10 @@ export async function createRoom(req, res) {
     if (!user) return res.status(401).send({ error: "Unauthorized" });
     const { name } = req.body;
     if (!name) return res.status(400).send({ error: "Missing name" });
+    // creates a new room chat
     const room = await dbClient.createRoom(user._id, name);
     if (!room) return res.status(500).send({ error: "internal server error" });
+    // sends the room info just created
     return res.status(201).send({ room });
   } catch (error) {
     console.log(error);
@@ -35,6 +39,11 @@ export async function joinRoom(req, res) {
     if (!user) return res.status(401).send({ error: "Unauthorized" });
     const roomId = req.params.id;
     if (!roomId) return res.status(404).send({ error: "Not Found" });
+    // check if user is already in the room
+    for (const joinedRoomId of user.rooms) {
+      if (roomId == joinedRoomId) return res.status(400).send({ error: "user already in this room" });
+    }
+    // joins a room: adds a room id to user.rooms and adds a user id to room.members
     const result = await dbClient.joinRoom(user._id, roomId);
     if (!result) return res.status(400).send({ error: "failed to join" });
     return res.status(200).send();
